fd9bc39a7272f379bcc715a86e0fbab1392021c7
anvesh1909 <m.anvesh190902@gmail.com>
2025-12-01 22:12:42 +0530

    updated entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,9 @@ cd /app
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py create_or_get_superuser
-gunicorn backend.wsgi:application --bind 0.0.0.0:"${PORT:-8000}"
+
+gunicorn backend.wsgi:application \
+  --bind 0.0.0.0:"${PORT:-8000}" \
+  --workers 2 \
+  --timeout 120 \
+  --log-file -
